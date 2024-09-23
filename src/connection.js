419ace8114499d6d5cc25fefd71d62b0fbaccc70
Ankit Kumar <134837821+akg78@users.noreply.github.com>
2024-09-23 14:49:42 +0530

    second commit

--- src/connection.js
+++ src/connection.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://ankitkumarkumar680:5nxSYtIwZIe3Vjyw@whiteboard78.qqxdu.mongodb.net/?retryWrites=true&w=majority&appName=Whiteboard78', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    //   useNewUrlParser: true,
     });
     console.log('MongoDB connected');
   } catch (error) {
