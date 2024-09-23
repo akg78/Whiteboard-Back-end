6bf58d3d33cb05fadf782a1d459f2899ad3fba72
Ankit Kumar <134837821+akg78@users.noreply.github.com>
2024-09-24 00:08:13 +0530

    updated routes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./src/connection');
@@ -6,16 +8,50 @@ const authRoutes = require('./src/authenticationRoute');
 const drawingRoutes = require('./src/drawingroutes');
 
 
+connectDB();
 const app = express();
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: (origin, callback) => {
+      if (!origin || origin.startsWith('http://localhost')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('draw', (pathData) => {
+    socket.broadcast.emit('draw', pathData);
+  });
+
+  socket.on('chatMessage', (message) => {
+    io.emit('chatMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+
+
 const PORT = process.env.PORT || 3001;
 
-connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/drawings', drawingRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -1,63 +1,63 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const authRoutes = require('./authenticationRoute');
-const drawingRoutes = require('./drawingroutes');
+// const express = require('express');
+// const http = require('http');
+// const socketIo = require('socket.io');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const authRoutes = require('./authenticationRoute');
+// const drawingRoutes = require('./drawingroutes');
 
-const app = express();
-const server = http.createServer(app);
+// const app = express();
+// const server = http.createServer(app);
 
-// Socket.io CORS for all localhost origins
-const io = socketIo(server, {
-  cors: {
-    origin: (origin, callback) => {
-      if (!origin || origin.startsWith('http://localhost')) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST'],
-    credentials: true,
-  },
-});
+// // Socket.io CORS for all localhost origins
+// const io = socketIo(server, {
+//   cors: {
+//     origin: (origin, callback) => {
+//       if (!origin || origin.startsWith('http://localhost')) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     methods: ['GET', 'POST'],
+//     credentials: true,
+//   },
+// });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
 
-  socket.on('draw', (pathData) => {
-    socket.broadcast.emit('draw', pathData);
-  });
+//   socket.on('draw', (pathData) => {
+//     socket.broadcast.emit('draw', pathData);
+//   });
 
-  socket.on('chatMessage', (message) => {
-    io.emit('chatMessage', message);
-  });
+//   socket.on('chatMessage', (message) => {
+//     io.emit('chatMessage', message);
+//   });
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
+//   socket.on('disconnect', () => {
+//     console.log('A user disconnected');
+//   });
+// });
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
 
-// Express CORS for all localhost origins
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || origin.startsWith('http://localhost')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
-app.use(bodyParser.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/drawings', drawingRoutes);
+// // Express CORS for all localhost origins
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin || origin.startsWith('http://localhost')) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+// }));
+// app.use(bodyParser.json());
+// app.use('/api/auth', authRoutes);
+// app.use('/api/drawings', drawingRoutes);
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
